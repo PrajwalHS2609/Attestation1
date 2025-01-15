86338422ff0041df6a3b86747ec0aa0d00deb17a
Prajwal H S <prajwalhs4india@gmail.com>
2025-01-15 20:25:21 +0530

    gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./global.css";
 import Navbar from "@/components/Navbar/Navbar";
 import TopBar from "@/components/TopBar/Topbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -11,20 +12,37 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" type="image/png" href="./favicon.png" />
+      <Head>
+        <title>Attestation Service</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        {/* ---gtag--- */}
-        <title>Attestation Service</title>
-      </head>
+        <meta
+          name="google-site-verification"
+          content="P9coYJ5EoFH_xlhaCCK_w7pQpa2g-n4UAEN1abymc4c"
+        />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-NK1W9JTDZ5"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-NK1W9JTDZ5');
+            `,
+          }}
+        ></script>
+      </Head>
       <body>
         <TopBar />
         <Navbar />
-        <main className="main-container"> {children}</main>
-        <Footer/>
+        <main className="main-container">{children}</main>
+        <Footer />
       </body>
     </html>
   );
