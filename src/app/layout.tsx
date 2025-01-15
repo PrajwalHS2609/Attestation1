941a61946b4300f90631ec9920c02d1f94648ccd
Prajwal H S <prajwalhs4india@gmail.com>
2025-01-15 20:36:09 +0530

    gtag changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,45 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Footer from "@/components/Footer/Footer";
+import "./global.css";
+import Navbar from "@/components/Navbar/Navbar";
+import TopBar from "@/components/TopBar/Topbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function Document() {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <Html lang="en">
-      <Head>
-        <title>Attestation Service</title>
+    <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="./favicon.png" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        <meta
-          name="google-site-verification"
-          content="P9coYJ5EoFH_xlhaCCK_w7pQpa2g-n4UAEN1abymc4c"
-        />
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-NK1W9JTDZ5"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-NK1W9JTDZ5');
-            `,
-          }}
-        ></script>
-      </Head>
+        
+        {/* Google Site Verification */}
+        <meta name="google-site-verification" content="P9coYJ5EoFH_xlhaCCK_w7pQpa2g-n4UAEN1abymc4c" />
+        
+        {/* Google Analytics (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NK1W9JTDZ5"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NK1W9JTDZ5');
+          `}
+        </script>
+
+        <title>Attestation Service</title>
+      </head>
       <body>
-        <Main />
-        <NextScript />
+        <TopBar />
+        <Navbar />
+        <main className="main-container"> {children}</main>
+        <Footer />
       </body>
-    </Html>
+    </html>
   );
 }
