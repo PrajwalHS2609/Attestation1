dc99db70de5a9d068a81c11614ff8e1f6e98ec84
Prajwal H S <prajwalhs4india@gmail.com>
2025-01-16 17:11:52 +0530

    g script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./global.css";
 import Navbar from "@/components/Navbar/Navbar";
 import TopBar from "@/components/TopBar/Topbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -17,12 +18,18 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        
+
         {/* Google Site Verification */}
-        <meta name="google-site-verification" content="P9coYJ5EoFH_xlhaCCK_w7pQpa2g-n4UAEN1abymc4c" />
-        
+        <meta
+          name="google-site-verification"
+          content="P9coYJ5EoFH_xlhaCCK_w7pQpa2g-n4UAEN1abymc4c"
+        />
+
         {/* Google Analytics (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NK1W9JTDZ5"></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-NK1W9JTDZ5"
+        ></script>
         <script>
           {`
             window.dataLayer = window.dataLayer || [];
@@ -32,7 +39,34 @@ export default function RootLayout({
           `}
         </script>
 
-        <title>Attestation Service</title>
+        {/* JSON-LD Structured Data */}
+        <Script id="product-schema" type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org/",
+            "@type": "Product",
+            "name": "Foreign Embassy Attestation",
+            "image": "https://foreignembassyattestation.com/_next/image?…Fmedia%2Fattestation1Logo.6c72ab9d.png&w=256&q=75",
+            "description":
+              "At Service Attestation1, we take pride in being a trusted leader in certificate attestation and apostille services. With over 15 years of expertise and a dedicated team of professionals, we have built a strong reputation for delivering fast, efficient, and reliable solutions.",
+            "brand": { "@type": "Thing", "name": "Service Attestion1" },
+            "review": {
+              "@type": "Review",
+              "reviewRating": {
+                "@type": "Rating",
+                "ratingValue": "5",
+                "bestRating": "5",
+              },
+              "author": { "@type": "Person", "name": "Rishi" },
+            },
+            "aggregateRating": {
+              "@type": "AggregateRating",
+              "ratingValue": "4.9",
+              "bestRating": "5",
+              "worstRating": "1",
+              "ratingCount": "8365",
+            },
+          })}
+        </Script>
       </head>
       <body>
         <TopBar />
