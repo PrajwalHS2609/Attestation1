96969505b4a6a7fbeec2eb956b6f623542f57e02
Prajwal H S <prajwalhs4india@gmail.com>
2025-04-25 19:01:33 +0530

    footer links

--- src/components/Footer/FooterApostille.tsx
+++ src/components/Footer/FooterApostille.tsx
@@ -15,31 +15,34 @@ export default function FooterApostille() {
           <Link href="germany-apostille-services-in-bangalore">Germany</Link>
         </li>
         <li>
-          <Link href="australia-apostille-services-in-bangalore">Australia</Link>
+          <Link href="australia-apostille-services-in-bangalore">
+            Australia
+          </Link>
         </li>
         <li>
-          <Link href="">Austria</Link>
+          <Link href="austria-apostille-services-in-bangalore">Austria</Link>
         </li>
         <li>
-          <Link href="">France</Link>
+          <Link href="france-apostille-services-in-bangalore">France</Link>
         </li>
         <li>
-          <Link href="">France</Link>
+          <Link href="lithuania-apostille-services-in-bangalore">
+            Lithuania
+          </Link>
         </li>
         <li>
-          <Link href="">Lithuania</Link>
+          <Link href="mexico-apostille-services-in-bangalore">Mexico</Link>
         </li>
         <li>
-          <Link href="">Mexico</Link>
+          <Link href="netherlands-apostille-services-in-bangalore">
+            Netherlands
+          </Link>
         </li>
         <li>
-          <Link href="">Netherlands</Link>
+          <Link href="uk-apostille-services-in-bangalore">UK</Link>
         </li>
         <li>
-          <Link href="">UK</Link>
-        </li>
-        <li>
-          <Link href="">USA</Link>
+          <Link href="usa-apostille-services-in-bangalore">USA</Link>
         </li>
       </ul>
     </div>
