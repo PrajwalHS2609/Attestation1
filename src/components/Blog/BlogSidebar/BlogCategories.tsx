7c3e2db7a9ed72d570f40ac453c487b847bb6f5b
Prajwal H S <prajwalhs4india@gmail.com>
2025-06-13 16:37:48 +0530

    menu

--- src/components/Blog/BlogSidebar/BlogCategories.tsx
+++ src/components/Blog/BlogSidebar/BlogCategories.tsx
@@ -1,24 +1,34 @@
 import Link from "next/link";
 import React from "react";
 import "@/components/Blog/Blog.css";
+import { client } from "@/sanity/client";
+import { SanityDocument } from "next-sanity";
 
-const BlogCategories = () => {
+const CATEGORY_QUERY = `*[_type == "blogCategory"] | order(title asc){
+  _id,
+  title,
+  slug
+}`;
+export default async function BlogCategories() {
+  const categories = await client.fetch<SanityDocument[]>(CATEGORY_QUERY);
   return (
     <div className="blogCategories-container">
       <ul>
         <h2>Categories</h2>
         <li>
-          <Link href={"/category/blog"}>All</Link>
+          <Link href={"/blog"}>All</Link>
         </li>
-        <li>
-          <Link href={"/category/attestation"}>Attestation</Link>
-        </li>
-        <li>
-          <Link href={"/category/apostille"}>Apostille</Link>
-        </li>
+       {categories
+          .filter((category) => category.slug && category.slug.current)
+          .map((category) => (
+            <li key={category._id}>
+              <Link href={`/category/${category.slug.current}`}>
+                {category.title}
+              </Link>
+            </li>
+          ))}
       </ul>
     </div>
   );
 };
 
-export default BlogCategories;
