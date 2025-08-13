fbad6f736bdc18e61141def7851789c2f7b86b93
Prajwal H S <prajwalhs4india@gmail.com>
2025-08-13 14:42:03 +0530

    table sanity added

--- src/components/Blog/BlogContentPages/EmbassyAttestationVsApostille/EmbassyAttestationVsApostilleImg.tsx
+++ src/components/Blog/BlogContentPages/EmbassyAttestationVsApostille/EmbassyAttestationVsApostilleImg.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 const EmbassyAttestationVsApostilleImg = () => {
   return (
     <div className="blogImg-content">
-      <img src={""} alt="Ten Hair Color Banner" loading="lazy" />
     </div>
   )
 }
