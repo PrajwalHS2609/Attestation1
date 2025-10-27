24fc147b6d1679686ca0b7b156749d869e008495
Prajwal H S <prajwalhs4india@gmail.com>
2025-10-27 17:07:19 +0530

    auto sitemap changes2

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,8 +1,8 @@
-export const SITEMAP_QUERY = `*[_type in ["page", "post", "serviceContent", "news"] && defined(slug.current)] {
+export const SITEMAP_QUERY = `*[_type in ["page", "post", "ServiceCategory", "news"] && defined(slug.current)] {
   "href": select(
     _type == "page" => "/" + slug.current,
     _type == "post" => "/" + slug.current,
-    _type == "serviceContent" => "/" + slug.current,
+    _type == "ServiceCategory" => "/" + slug.current,
     _type == "news" => "/" + slug.current,
     "/" + slug.current
   ),
