cd755b13a038be52c3c2957d2a551b7a4c15fd0e
Prajwal H S <prajwalhs4india@gmail.com>
2025-10-29 13:53:16 +0530

    changes in slug

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { notFound } from "next/navigation";
 import { client } from "@/sanity/client";
-import PostContent, { PostContentType } from "@/components/SanityComponents/Post";
-import ServiceContent, { ServiceContentType } from "@/components/SanityComponents/ServiceContent";
-import NewsContent, { NewsContentType } from "@/components/SanityComponents/NewsContent";
-
+import PostContent, {
+  PostContentType,
+} from "@/components/SanityComponents/Post";
+import ServiceContent, {
+  ServiceContentType,
+} from "@/components/SanityComponents/ServiceContent";
+import NewsContent, {
+  NewsContentType,
+} from "@/components/SanityComponents/NewsContent";
 
 const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]{_id, title, slug, body, mainImage { asset->{ url } }, youtubeVideoUrl, faq[] { question, answer }}`;
-const SERVICE_QUERY = `*[_type == "serviceContent" && slug.current == $slug][0]{_id, title, slug, body1, body2, mainImage { asset->{ url } }, youtubeVideoUrl, faq[] { question, answer }, customTable{title, headers, rows[] {cells}}}`;
+const SERVICE_QUERY = `*[_type == "ServiceCategory" && slug.current == $slug][0]{_id, title, slug, body1, body2, mainImage { asset->{ url } }, youtubeVideoUrl, faq[] { question, answer }, customTable{title, headers, rows[] {cells}}}`;
 const NEWS_QUERY = `*[_type == "news" && slug.current == $slug][0]{_id, title, slug, body, author, publishedAt, mainImage { asset->{ url } }, faq[] { question, answer }}`;
 
 export const revalidate = 0;
@@ -21,13 +26,20 @@ export default async function SlugPage({
 }) {
   const { slug } = await paramsPromise;
 
-  const post: PostContentType | null = await client.fetch<PostContentType>(POST_QUERY, { slug });
+  const post: PostContentType | null = await client.fetch<PostContentType>(
+    POST_QUERY,
+    { slug }
+  );
   if (post) return <PostContent content={post} />;
 
-  const service: ServiceContentType | null = await client.fetch<ServiceContentType>(SERVICE_QUERY, { slug });
+  const service: ServiceContentType | null =
+    await client.fetch<ServiceContentType>(SERVICE_QUERY, { slug });
   if (service) return <ServiceContent content={service} />;
 
-  const news: NewsContentType | null = await client.fetch<NewsContentType>(NEWS_QUERY, { slug });
+  const news: NewsContentType | null = await client.fetch<NewsContentType>(
+    NEWS_QUERY,
+    { slug }
+  );
   if (news) return <NewsContent content={news} />;
 
   notFound();
