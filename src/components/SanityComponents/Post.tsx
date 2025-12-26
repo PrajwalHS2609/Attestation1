0d93dbfe9a9504e8d4c06522ae532cb63f526931
Prajwal H S <prajwalhs4india@gmail.com>
2025-12-26 13:45:15 +0530

    changes in sanity clickable

--- src/components/SanityComponents/Post.tsx
+++ src/components/SanityComponents/Post.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 import { PortableText } from "next-sanity";
 import { portableTextComponents } from "@/components/PortableTextComponents";
